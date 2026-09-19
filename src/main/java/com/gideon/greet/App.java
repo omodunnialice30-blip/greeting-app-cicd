ec12da904bc58bd8ffbebf8f08d5a85ac78a59dd
omodunni <omodunnialice30@gmail.com>
2026-09-19 13:53:14 +0100

    Update greeting message to test auto-trigger

--- src/main/java/com/gideon/greet/App.java
+++ src/main/java/com/gideon/greet/App.java
@@ -1,7 +1,1 @@
-package com.gideon.greet;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from Gideon's CI/CD Pipeline!");
-    }
-}
+System.out.println("CI/CD is now fully automated!");
